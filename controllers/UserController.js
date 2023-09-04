202dfac14e3d42dd86fe0801172e09ba3e1f38c1
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-09-04 22:28:40 +0100

    Update UserController.js

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -40,7 +40,7 @@ const loginUser=async(req,res)=>{
 const {email,password}=req.body //The values collected from the request body/frontend
 if (!email || !password) { //If a field or both is left empty, it'll return this message and won't proceed to the next function
 return res.status(400).json({error:"All fields must be filled"})
-    }
+}
     if (!validator.isEmail(email)) {  //Checks if the email entered is valid or not
         return res.status(400).json({error:"Invalid email format"})      
     }
