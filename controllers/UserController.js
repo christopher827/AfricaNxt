2cf4c3853c4749d319db4089acd030652a8c4c16
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-09-12 00:04:01 +0100

    Update UserController.js

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -45,7 +45,7 @@ if (!validator.isEmail(email)) {  //Checks if the email entered is valid or not
 return res.status(400).json({error:"Invalid email format"})      
 }
 if (!validator.isStrongPassword(password)) { //Checks if the password entered is strong enough or not
-        return res.status(400).json({error:"Password is not strong enough"})
+return res.status(400).json({error:"Password is not strong enough"})
     }
     const user=await User.findOne({email}) //Checks if the email is in our database
 if (!user) {  //if the email is not, it returns this message
